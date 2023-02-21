fefe2e16b5bdbcbe68fc74cea7d25034f7d155e8
Jose Luis <josemr.99@outlook.com>
2023-02-20 23:37:02 -0500

    fast api baldolov

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import bodyParser from "body-parser";
 import express from "express";
-import pg from "pg";
+import users from './db/users.json';
+import { writeFile } from 'fs/promises'
+// import pg from "pg";
 
 // Connect to the database using the DATABASE_URL environment
 //   variable injected by Railway
-const pool = new pg.Pool();
+// const pool = new pg.Pool();
+
 
 const app = express();
 const port = process.env.PORT || 3333;
@@ -13,11 +16,36 @@ app.use(bodyParser.json());
 app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
 app.use(bodyParser.text({ type: "text/html" }));
 
-app.get("/", async (req, res) => {
-  const { rows } = await pool.query("SELECT NOW()");
-  res.send(`Hello, World! The time from the DB is ${rows[0].now}`);
+// const { rows } = await pool.query("SELECT NOW()");
+app.get("/users", async (req, res) => {
+  res.json(users);
+});
+
+app.get("/users/:dni", async (req, res) => {
+  const { dni } = req.params
+  const user = users.find(u => u.dni === dni)
+  if(!user){
+    return res.status(404).json({
+      "message": "User not found"
+    })
+  }
+  res.json(user);
+});
+
+app.post("/users", async (req, res) => {
+  const { name, dni, address, email, birthday  } = req.body
+  if(users.some(u => u.dni === dni)){
+    return res.status(409).json({
+      "message": "User already exists"
+    })
+  }
+  const user = { name, dni, address, email, birthday }
+  users.push(user)
+  await writeFile('./src/db/users.json', JSON.stringify(users))
+  res.json(user);
 });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
